e480daacc3d93644bcdce4a8ddb49103442295ae
MSI <MSI@DESKTOP-Q06R7EA>
2025-03-15 12:22:08 +0200

    Добавьте файлы проекта.

---
+++ TestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=======Mathematical action=======");
+
+            Console.Write("Enter num1: ");
+            decimal num1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Enter num2: ");
+
+            decimal num2 = Convert.ToDecimal(Console.ReadLine());
+
+
+            Console.WriteLine($"Result:{num1+num2}");
+
+        }
+    }
+}
